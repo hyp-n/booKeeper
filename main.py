44b6400050c0e9941a49ec7219c0607123aeea79
hyp-n <siddharth1210@outlook.com>
2026-09-21 22:03:28 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, redirect, url_for
+from markupsafe import escape
+
+app=Flask(__name__)
+
+#Index Page
+@app.route('/')
+def index():
+    #TODO:Make the username render here frfr
+    return render_template("index.html")
+    
+#Book Collection Page
+@app.route('/col')
+def collection():
+    return render_template("collections.html")
+
+#Page count page
+@app.route('/pgcount')
+def pgcount():
+    return render_template("pagecount.html")
+
+#TODO:Integrate Login and Usernames
+
+
+
+#Runs the server without using the full command, running this file is enough
+if __name__ == "__main__":
+    app.run(debug=True)
