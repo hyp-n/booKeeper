12ebf2754be3f149c51a67224cebb90928fb30c3
hyp-n <siddharth1210@outlook.com>
2026-09-25 18:46:38 +0530

    uhm more stuff chaged, esp. syntax errors and incorrect usage of stuffies

--- main.py
+++ main.py
@@ -1,46 +1,67 @@
+from flask import Flask, render_template, redirect, url_for, request as flask_request
+from markupsafe import escape
+import json
+from xtramodules import isbn_fetcher, save_book_data
+import urllib.request
 import os
 from dotenv import load_dotenv
-from flask import Flask, render_template, request
-from xtramodules import isbn_fetcher
+from bson.objectid import ObjectId
+
 
 load_dotenv()
 
-bKpr = Flask(__name__)
-bKpr.secret_key = os.getenv("SECRET_KEY")
+app=Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
 
 
-@bKpr.route("/")
+
+#Index Page
+@app.route('/')
 def index():
-  return render_template("index.html", username="hyp-en")
+    #TODO:Make the username render here frfr
+    username = "hyp-en"
+    return render_template("index.html", username=username)
 
+#lowk i made this to test stuff out , and I will NEVER remove stuff i tested unless explicitly mentioned so enjou
+@app.route('/scrathpad')
+def scratchpad():
+    return render_template("scratchpad.html")
 
-@bKpr.route("/collections")
+#Book Collection Page
+@app.route('/collections')
 def collection():
-  # TODO: Fetch collections from MongoDB and group by collection name
-  return render_template("collections.html")
+    return render_template("collections.html")
 
+#Page count page
+@app.route('/pgcount')
+def pgcount():
+    return render_template("pagecount.html")
 
-@bKpr.route("/add-book", methods=["POST"])
+#book adding
+@app.route("/add-book", methods=["POST"])
 def add_book_handler():
-  isbn = request.form.get("isbn")
+  isbn = flask_request.form.get("isbn")
 
-  if not isbn:
-    return "<p class='error'>ISBN is required</p>", 400
-
-  # Save book using helper function
-  new_book = isbn_fetcher(isbn)
-
-  # HTMX Partial Swap: Return just the new book HTML card
-  if request.headers.get("HX-Request"):
-    return render_template("partials/book_card.html", book=new_book)
+  if isbn:
+    new_book = save_book_data(isbn)
+    if flask_request.headers.get("HX-Request"):
+      return render_template("partials/book_card.html", book=new_book)
 
   return redirect(url_for("collection"))
 
+#book moving
+@app.route("/api/movebook", method="POST")
+def book_move():
+   
+   return pass
 
-@bKpr.route("/scrathpad")
-def scratchpad():
-  return render_template("scratchpad.html")
+#TODO:Integrate Login and Usernames
+
+#--- Uh Library ISBN Stuff ---#
 
 
+
+
+#Runs the server without using the full command, running this file is enough
 if __name__ == "__main__":
-  bKpr.run(debug=True)
+    app.run(debug=True)
