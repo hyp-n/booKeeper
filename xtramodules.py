1a5407088eea89a4e91e37ab70bccda33bb074f6
hyp-n <siddharth1210@outlook.com>
2026-09-25 21:21:19 +0530

    VERSION 0.0.2 - 1st major Refactor

--- xtramodules.py
+++ xtramodules.py
@@ -1,10 +1,12 @@
 import json
+import time
 import urllib.request
 import pymongo
 import os
 from dotenv import load_dotenv
 from bson.objectid import ObjectId
 from flask import render_template, request
+from pymongo.errors import DuplicateKeyError
 
 load_dotenv()
 
@@ -16,8 +18,12 @@ def isbn_fetcher(isbn):
         headers={'User-Agent': 'booKeeperApp/0.1 (educational project)'}
     )
     try:
+        start = time.time()
+
         with urllib.request.urlopen(req) as response:
+            print("OL connection/response:", time.time() - start)
             book_meta = json.loads(response.read().decode('utf-8'))
+            print("OL total:", time.time() - start)
 
         if book_meta:
             title = book_meta.get('title')
@@ -56,18 +62,27 @@ def isbn_fetcher(isbn):
         return 'No Book Found!'
 
 def save_book_data(isbn):
+    start = time.time()
     book_data = isbn_fetcher(isbn)
+    print(f"OL: {time.time()-start}")
     if isinstance(book_data, dict):
+        start = time.time()
         myclient = pymongo.MongoClient(os.getenv("MONGO_URI"))
         mydb = myclient["mydatabase"] 
         total_books = mydb["all_books"]   
-        total_books.insert_one(book_data)
-        return True
+        try:
+            result = total_books.insert_one(book_data)
+        except DuplicateKeyError:
+            return False
+        book_data["_id"] = result.inserted_id
+        print(f"MDB: {time.time() - start}")  
+        return book_data
     return False
 
 def get_books_collection():
     client = pymongo.MongoClient(os.getenv("MONGO_URI"))
-    return client["mydatabase"]["all_books"]
+    books = client["mydatabase"]["all_books"]
+    return books
 
 def get_collections():
     books_col = get_books_collection()
